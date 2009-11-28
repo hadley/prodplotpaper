6eaf75061c05b50486eb3434e07f402cd388e038
hadley <h.wickham@gmail.com>
2009-11-28 11:38:23 -0600

    More working

--- plots-partition.r
+++ plots-partition.r
@@ -1,20 +1,27 @@
-eq <- data.frame(trt = letters[1:4], .wt = rep(0.25, 4))
 vr <- data.frame(trt = letters[1:4], .wt = c(0.5, 0.2, 0.2, 0.1))
 
-prodplot(eq, ~ trt, "vbar")
-ggsave("plots/part-vbar-eq.pdf", width = 4, height = 4)
-prodplot(vr, ~ trt, "vbar")
-ggsave("plots/part-vbar-vr.pdf", width = 4, height = 4)
-prodplot(eq, ~ trt, "hbar")
-ggsave("plots/part-hbar-eq.pdf", width = 4, height = 4)
 prodplot(vr, ~ trt, "hbar")
-ggsave("plots/part-hbar-vr.pdf", width = 4, height = 4)
+ggsave("plots/part-hbar.pdf", width = 4, height = 4)
 
-prodplot(eq, ~ trt, "vspline")
-ggsave("plots/part-vspline-eq.pdf", width = 4, height = 4)
-prodplot(vr, ~ trt, "vspline")
-ggsave("plots/part-vspline-vr.pdf", width = 4, height = 4)
-prodplot(eq, ~ trt, "hspline")
-ggsave("plots/part-hspline-eq.pdf", width = 4, height = 4)
 prodplot(vr, ~ trt, "hspline")
-ggsave("plots/part-hspline-vr.pdf", width = 4, height = 4)
+ggsave("plots/part-hspline.pdf", width = 4, height = 4)
+
+prodplot(vr, ~ trt, "treemap")
+ggsave("plots/part-treemap.pdf", width = 4, height = 4)
+
+
+# 2d partition
+prodplot(happy, ~ happy + finrela, c("vspline", "hspline")) + 
+  aes(fill = finrela)
+prodplot(happy, ~ happy + health, c("vspline", "hspline")) + 
+  aes(fill = health)
+prodplot(happy, ~ happy + marital, c("vspline", "hspline")) + 
+  aes(fill = marital)
+
+prodplot(happy, ~ happy + finrela + health, c("vspline", "fluct"), 
+  colour = NA, na.rm = TRUE) + aes(fill = happy)
+ggsave("plots/part-fluct.pdf", width = 6, height = 6)
+prodplot(happy, ~ happy | finrela + health, c("vspline", "fluct"), 
+  colour = NA, na.rm = TRUE) + aes(fill = happy)
+ggsave("plots/part-fluct-cond.pdf", width = 6, height = 6)
+
