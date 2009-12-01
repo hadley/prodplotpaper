ecaf10e36b5c07ae52836a7b13110223311ea802
hadley <h.wickham@gmail.com>
2009-12-01 13:33:49 -0600

    Better equal bin size example

--- plots-partition.r
+++ plots-partition.r
@@ -17,12 +17,22 @@ ggsave("plots/part-treemap.pdf", width = 4, height = 4)
 
 # 2d partition
 
+# Find combinations with most zeros
+comb <- subset(expand.grid(x = 2:9, y = 2:9), x > y)
+comb$x <- names(happy)[comb$x]
+comb$y <- names(happy)[comb$y]
+subset(mdply(comb, function(x, y) {
+  sum(table(happy[[x]], happy[[y]]) == 0)
+}), V1 > 0)
+
+
+
 happy$decade <- cut(happy$age, seq(10, 90, by = 10))
 prodplot(happy, ~  decade + marital, "fluct", na.rm = T) + 
   aes(fill = marital)
 ggsave("plots/part-fluct.pdf", width = 6, height = 6)
-prodplot(happy, ~ . |  decade + marital, "fluct", na.rm = T) + 
-  aes(fill = marital)
+prodplot(happy, ~ . | year + health, "fluct") + 
+  aes(fill = health)
 ggsave("plots/part-fluct-cond.pdf", width = 6, height = 6)
 
 
