b75d4bd3900af1618e10174bfec4bb998f54d9e4
hadley <h.wickham@gmail.com>
2009-12-04 09:35:55 -0600

    nd partition section

--- plots-partition.r
+++ plots-partition.r
@@ -1,4 +1,7 @@
 l(productplots)
+source("colour.r")
+
+# 1d partitions --------------------------------------------------------------
 
 prodplot(happy, ~ happy, "hbar", colour = "grey50", na.rm = TRUE) + fill_happy
 ggsave("plots/part-hbar.pdf", width = 4, height = 4)
@@ -9,7 +12,7 @@ ggsave("plots/part-hspine.pdf", width = 4, height = 4)
 prodplot(happy, ~ happy, "treemap", na.rm = TRUE) + fill_happy
 ggsave("plots/part-treemap.pdf", width = 4, height = 4)
 
-# 2d partition
+# 2d partitions --------------------------------------------------------------
 
 # Find combinations with most zeros
 comb <- subset(expand.grid(x = 2:9, y = 2:9), x > y)
@@ -20,24 +23,40 @@ subset(mdply(comb, function(x, y) {
 }), V1 > 0)
 
 
-
+# age & marital status
 happy$decade <- cut(happy$age, seq(10, 90, by = 10))
 prodplot(happy, ~  decade + marital, "fluct", na.rm = T) + 
   aes(fill = marital)
 ggsave("plots/part-fluct.pdf", width = 6, height = 6)
-prodplot(happy, ~ . | year + health, "fluct") + 
+
+# year & health
+prodplot(happy, ~ . | year + health, "fluct", na.rm = TRUE) + 
   aes(fill = health)
 ggsave("plots/part-fluct-cond.pdf", width = 6, height = 6)
 
 
-# nd partition
+# nd partitions --------------------------------------------------------------
 
+# show partitioning process
+df <- prodcalc(happy, ~ happy + sex + marital, na.rm = TRUE, 
+  set_offset(c("vspine", "hspine", "hspine"), c(0, 0, 0.01)))
+
+draw(subset(df, level == 1)) + aes(fill = marital)
+ggsave("plots/part-comb-1.pdf", width = 6, height = 6)
+
+draw(subset(df, level == 2)) + aes(fill = sex)
+ggsave("plots/part-comb-2.pdf", width = 6, height = 6)
+
+draw(subset(df, level == 3)) + fill_happy
+ggsave("plots/part-comb-3.pdf", width = 6, height = 6)
+
+
+# alternative displays of marital status & age
 prodplot(happy, ~ marital + decade, set_offset(c("vspine", "hspine"), c(0, 0.02)), na.rm = T) + aes(fill = marital)
 ggsave("plots/part-marital-1.pdf", width = 6, height = 6)
 
-prodplot(happy, ~ marital | decade, set_offset(c("vspine", "hspine"), c(0, 0.02)), na.rm = T) + aes(fill = marital)
+prodplot(happy, ~ decade + marital, set_offset(c("hbar", "vspine"), c(0, 0.02)), na.rm = T) + aes(fill = marital)
 ggsave("plots/part-marital-2.pdf", width = 6, height = 6)
-
 
 prodplot(happy, ~ happy + finrela, c("vspine", "hspine")) + 
   aes(fill = finrela)
@@ -46,10 +65,11 @@ prodplot(happy, ~ happy + health, c("vspine", "hspine")) +
 prodplot(happy, ~ happy + marital, c("vspine", "hspine")) + 
   aes(fill = marital)
 
+# fluctuation & equal bin size
 prodplot(happy, ~ happy + finrela + health, c("vspine", "fluct"), 
   colour = NA, na.rm = TRUE) + fill_happy
-ggsave("plots/part-nd-1.pdf", width = 6, height = 6)
+ggsave("plots/part-fluctuation.pdf", width = 6, height = 6)
 
 prodplot(happy, ~ happy | finrela + health, c("vspine", "fluct"), 
   colour = NA, na.rm = TRUE) + fill_happy
-ggsave("plots/part-nd-2.pdf", width = 6, height = 6)
+ggsave("plots/part-equal-area.pdf", width = 6, height = 6)
