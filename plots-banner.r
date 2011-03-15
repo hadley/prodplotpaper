e769eecb0ecfe046a4c4b65c055228e3672e8307
hadley <h.wickham@gmail.com>
2011-03-15 15:41:20 -0500

    Tweaking banner

--- plots-banner.r
+++ plots-banner.r
@@ -9,13 +9,13 @@ ggsave("plots/banner-1.pdf", width = 4, height = 4)
 prodplot(happy, ~ marital + happy, c("tile", "tile"), na.rm = TRUE) + fill_marital + no_axes
 ggsave("plots/banner-2.pdf", width = 4, height = 4)
 
-prodplot(happy, ~ marital + happy, c("hbar", "vbar"), na.rm = TRUE) + fill_marital + no_axes
+prodplot(happy, ~ marital + happy, c("hbar", "vbar"), na.rm = TRUE, levels = NA) + fill_marital + no_axes
 ggsave("plots/banner-3.pdf", width = 4, height = 4)
 
-prodplot(happy, ~ marital + happy, c("hbar", "hbar"), na.rm = TRUE) + fill_marital + no_axes
+prodplot(happy, ~ marital + happy, c("hbar", "hspine"), na.rm = TRUE, levels = NA) + fill_marital + no_axes
 ggsave("plots/banner-4.pdf", width = 4, height = 4)
 
-prodplot(happy, ~ marital + happy, c("hspine", "vbar"), na.rm = TRUE) + fill_marital + no_axes
+prodplot(happy, ~ marital + happy, c("hspine", "vbar"), na.rm = TRUE, levels = NA) + fill_marital + no_axes
 ggsave("plots/banner-5.pdf", width = 4, height = 4)
 
 prodplot(happy, ~ marital + happy, c("fluct"), na.rm = TRUE) + fill_marital + no_axes
